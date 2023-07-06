cb391516fb7272df92bb078493395a2034609a9b
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2023-07-06 11:11:59 -0500

    Initial Commit
    stood up homepage with nav, header, & card links
    added styling too

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
+  <router-view/>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
 </template>
 
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '@/styles/colors.scss';
+@import '@/styles/main.scss';
+@import '@/styles/header.scss';
+@import '@/styles/nav.scss';
+@import '@/styles/cards.scss';
+@import '@/styles/logo.scss';
 </style>
---
+++ src/components/WisLogo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="logo-wrapper">
+        <img src="@/assets/wis_logo.svg" alt="logo">
+        <h1 class="title">Written in Stone</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WisLogo',
+}
+</script>
